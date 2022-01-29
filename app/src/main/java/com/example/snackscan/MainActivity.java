4df82087ff6ad50c4006818c0553df0d359da800
anashen <anaghas2@illinois.edu>
2022-01-29 14:35:45 -0600

    Add login/register screens and authentication

--- app/src/main/java/com/example/snackscan/MainActivity.java
+++ app/src/main/java/com/example/snackscan/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.snackscan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        finish();
+    }
+
 }
