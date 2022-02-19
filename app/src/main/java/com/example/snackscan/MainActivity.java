afe124a31e59b317e3aa28c9bf50b39f6c3fb1e0
anashen <anaghas2@illinois.edu>
2022-02-19 16:44:39 -0600

    Remove merge conflict messages

--- app/src/main/java/com/example/snackscan/MainActivity.java
+++ app/src/main/java/com/example/snackscan/MainActivity.java
@@ -3,12 +3,10 @@ package com.example.snackscan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-<<<<<<< HEAD
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
-=======
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -25,7 +23,6 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Random;
->>>>>>> main
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 101;
@@ -53,12 +50,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-<<<<<<< HEAD
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
-=======
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -68,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             iview.setImageBitmap(imageBitmap);
         }
->>>>>>> main
+
     }
 
 }
