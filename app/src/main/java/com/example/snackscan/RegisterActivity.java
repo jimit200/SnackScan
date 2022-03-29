bea05f6047e626eea8fe97e692bfbd6209809ed3
anashen <anaghas2@illinois.edu>
2022-03-29 18:51:21 -0500

    Fix more errors

--- app/src/main/java/com/example/snackscan/RegisterActivity.java
+++ app/src/main/java/com/example/snackscan/RegisterActivity.java
@@ -57,7 +57,6 @@ public class RegisterActivity extends AppCompatActivity {
             finish();
         }
 
-
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
