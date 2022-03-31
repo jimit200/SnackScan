a6355d838ce6188061e54bbe454cf15120c3c5f4
Jimit <jimit.gosar200@gmail.com>
2022-03-31 17:02:31 -0500

    Added screen to add ingredients to avoid

---
+++ app/src/main/java/com/example/snackscan/EnterAllergiesActivity.java
@@ -0,0 +1,66 @@
+package com.example.snackscan;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class EnterAllergiesActivity extends AppCompatActivity {
+
+    EditText mAllergies;
+    Button mSubmitButton;
+    FirebaseFirestore fStore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_allergies);
+        fStore = FirebaseFirestore.getInstance();
+        mAllergies = findViewById(R.id.editTextAllergies);
+        mSubmitButton = findViewById(R.id.submitButton);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                String id = intent.getStringExtra("user_id");
+                String allergies = mAllergies.getText().toString();
+
+                DocumentReference documentReference = fStore.collection("users").document(id);
+                documentReference.update("Allergies", allergies).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "onSuccess: user allergies added for " + id);
+                    }
+                });
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
